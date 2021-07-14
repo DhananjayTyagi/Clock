b111f48959bf97a1b870ccd708b0cd21bf935ab9
Dhananjay Dev Tyagi <70812181+DhananjayTyagi@users.noreply.github.com>
2021-07-14 11:51:58 +0530

    Add files via upload

---
+++ Clock.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import time
+
+root = Tk()
+root.configure(bg = "navajo white")
+
+label1 = Label(root,font = 'comicsans 50',bg = "navajo white")
+label1.pack()
+
+def get_time():
+    current_time = time.strftime("%H:%M:%S  %p")
+    label1.configure(text=current_time)
+    label1.after(200,get_time)
+
+get_time()
+root.mainloop()
